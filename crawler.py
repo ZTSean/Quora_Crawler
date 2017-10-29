96e41ec20150bb42396dcfe1b1f779c00908aca2
Sean Wang <wzx411098871@gmail.com>
2017-10-29 01:33:29 -0400

    Quora Crawler version 1.0

---
+++ crawler.py
@@ -0,0 +1,145 @@
+from user import User
+from nquora import Quora
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import codecs
+import time
+import Queue
+
+## Settings -----------------------------------
+username = 'exmaple@gmail.com'
+password = '123456'
+total_users_to_read = 2000
+current_read_users = 0
+users_to_read_queue = Queue.Queue()
+user_stats = [] ## final users stats to store
+
+## Get answer for top users in Quora in 2017
+url = 'https://www.quora.com'
+question = 'Which-are-the-top-10-most-followed-accounts-on-Quora'
+author = 'Anmol-Jamwal-2'
+answer = Quora.get_one_answer(question, author) # question and answer are variables
+
+print '---------------------------------'
+### READING USERS INFO UNTIL TOTAL NUMBER REACH THE THRESHOLD ===================================
+## Search users in the list of answer
+print 'Start collecting user name of top users from dictionary...'
+soup = BeautifulSoup(answer['answer'], 'html.parser')
+
+user_span = soup.find_all('span', class_='qlink_container')
+
+user_name_list = []
+for item in user_span:
+    name = str(item.find('a', href=True)['href'].split('/')[2])
+    #user_name_list.append(name)
+    users_to_read_queue.put(name)
+
+#users_to_read_queue.put(user_name_list[1])
+
+print users_to_read_queue.qsize()
+
+print "Got all top user names..."
+print '---------------------------------'
+print 'Start collecting user profile data...'
+
+driver = webdriver.PhantomJS()
+driver.get('http://www.quora.com/profile/Zixiao-Wang-2/followers')
+
+## check log in
+User.check_login(driver, username, password)
+
+while not users_to_read_queue.empty():
+    ## every time read k(total number of people in the list) people, read level by level
+    for i in range(users_to_read_queue.qsize()):
+        name = users_to_read_queue.get()
+        print name
+        user = User(name)
+        if user.stats:
+            user_stats.append(user.stats)
+            current_read_users = current_read_users + 1
+
+            ## append his/her followers to the to_read_queue
+
+            followers = User.get_followers(driver, name, username, password)
+            print "# of followers for " + name + ": " + str(len(followers))
+            map(users_to_read_queue.put, followers)
+        '''
+            for un in followers:
+                print un
+        
+        if i > 1: ## test use
+            break
+        '''
+        print current_read_users
+        if current_read_users > total_users_to_read:
+            break
+
+'''
+
+for user_name in user_name_list:
+    i = i+1
+    print i
+    user = User(user_name)
+    if user.stats:
+        user_stats.append(user.stats)
+        current_read_users = current_read_users + 1
+    if i > 1:
+        break
+
+'''
+print 'Done with collecting user stats: ' + str(len(user_stats))
+
+
+print '---------------------------------'
+### STORE DATA TO LOCAL FILE ===================================
+print 'Start writing collected data into local file...'
+
+
+output_file_name = 'quora_user_stats.csv'
+f = open(output_file_name, 'w')
+
+## write headers to the csv file
+f.write('username,edits,followers,name,questions,following,blogs,posts,topics,answers\n')
+for s in user_stats:
+    f.write(s['username'] + ',')
+    f.write(str(s['edits']) + ',')
+    f.write(str(s['followers']) + ',')
+    f.write(s['name'] + ',')
+    f.write(str(s['questions']) + ',')
+    f.write(str(s['following']) + ',')
+    f.write(str(s['blogs']) + ',')
+    f.write(str(s['posts']) + ',')
+    f.write(str(s['topics']) + ',')
+    f.write(str(s['answers']) + '\n')
+f.close()
+print 'Done with local file writing...'
+
+'''
+print '---------------------------------'
+print 'Start collecting followers info...'
+user = User(author)
+print len(user.followers)
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.quora.com/profile/Robert-Scoble-1/followers')
+
+
+
+
+
+time.sleep(1)
+driver.get('https://www.quora.com/profile/Robert-Scoble-1/followers')
+
+f = codecs.open('followers.log', 'w', 'utf-8')
+f.write(BeautifulSoup(driver.page_source).prettify())
+f.close()
+
+## grad each follower's info, append to queue
+soup = BeautifulSoup(driver.page_source)
+soup.find('div', class_='UserConnectionsFollowersList').find_all('a', class_='user', href=True)
+
+'''
+
+
+driver.close()
---
+++ user.py
@@ -0,0 +1,241 @@
+#coding=utf-8
+
+from bs4 import BeautifulSoup
+from quora import try_cast_int
+import feedparser
+import re
+import requests
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+from selenium import webdriver
+
+### Configuration ###
+POSSIBLE_FEED_KEYS = ['link', 'id', 'published', 'title', 'summary']
+
+### Enumerated Types ###
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    reverse = dict((value, key) for key, value in enums.iteritems())
+    enums['reverse_mapping'] = reverse
+    return type('Enum', (), enums)
+
+ACTIVITY_ITEM_TYPES = enum(UPVOTE=1, USER_FOLLOW=2, WANT_ANSWER=3, ANSWER=4, REVIEW_REQUEST=5)
+LOG_ENTRY_TYPES     = enum(ANSWER_ADDED=1, ANSWER_DELETED=2, COMMENT=2, EDIT=3, TOPIC=4)
+
+####################################################################
+# Helpers
+####################################################################
+
+def get_name(source):
+    return str(source.find('span', attrs={'class' : 'user'}).string)
+
+def build_feed_item(item):
+    result = {}
+    keys = POSSIBLE_FEED_KEYS
+    for key in keys:
+        if key in item.keys():
+            result[key] = item[key]
+    return result
+
+def is_want_answer(description):
+    tag  = description.find('span', id = re.compile('^[a-z]*_+[a-z]*_+[0-9]*$'))
+    if tag is not None:
+        return True
+    else:
+        return False
+
+def is_author(link, baseurl):
+    author = re.search('[a-zA-Z-\-]*\-+[a-zA-Z]*-?[0-9]*$', link)
+    user   = re.search('com*\/([a-zA-Z]*\-+[a-zA-Z]*-?[a-z-A-Z-0-9]*)\/rss$', baseurl)
+    if user is not None and author is not None:
+        author = author.group(0)
+        user   = user.group(1)
+        return author == user
+    else:
+        return False
+
+def is_review(link):
+    if link is not None:
+        match = re.search('^https?:\/\/www\.?quora.com\/Reviews-of[a-zA-Z0-9-\-]*$', link)
+        if match is not None:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def check_activity_type(entry):
+    description = BeautifulSoup(entry['description'])
+    link        = entry['link']
+    base_url    = entry['summary_detail']['base']
+
+    if entry['description'] == '':
+        return ACTIVITY_ITEM_TYPES.USER_FOLLOW
+    elif is_review(link) is True:
+        return ACTIVITY_ITEM_TYPES.REVIEW_REQUEST
+    elif is_want_answer(description) is True:
+        return ACTIVITY_ITEM_TYPES.WANT_ANSWER
+    elif is_author(link, base_url) is True:
+        return ACTIVITY_ITEM_TYPES.ANSWER
+    else:
+        return ACTIVITY_ITEM_TYPES.UPVOTE
+
+
+####################################################################
+# API
+####################################################################
+class User:
+    def __init__(self, user):
+        self.user = user
+        self._stats = None
+        self._activity = None
+        self._followers = []
+
+    @property
+    def stats(self):
+        if self._stats is None:
+            self._stats = self.get_user_stats(self.user)
+        return self._stats
+
+    @property
+    def activity(self):
+        if self._activity is None:
+            self._activity = self.get_user_activity(self.user)
+        return self._activity
+
+    @property
+    def followers(self):
+        if self._followers is None:
+            self._followers = self.get_followers(self.user)
+        return self._followers
+
+    @staticmethod
+    def get_user_stats(user):
+        try:
+            soup = BeautifulSoup(requests.get('http://www.quora.com/' + user).text)
+            data_stats = []
+            name = get_name(soup)
+            err = None
+
+            for item in soup.find_all('span', attrs={'class' : 'list_count'}):
+                data_stats.append(item.string)
+            data_stats = map(try_cast_int, data_stats)
+
+            user_dict = {'answers'   : data_stats[0],
+                         'blogs'     : data_stats[3],
+                         'edits'     : data_stats[7],
+                         'followers' : data_stats[4],
+                         'following' : data_stats[5],
+                         'name'      : name,
+                         'posts'     : data_stats[2],
+                         'questions' : data_stats[1],
+                         'topics'    : data_stats[6],
+                         'username'  : user }
+            return user_dict
+        except:
+            return {}
+
+    @staticmethod
+    def get_user_activity(user):
+        try:
+            f = feedparser.parse('http://www.quora.com/' + user + '/rss')
+            result = {
+                'username': user,
+                'last_updated': f.feed.updated
+            }
+            for entry in f.entries:
+                if 'activity' not in result.keys():
+                    result['activity'] = []
+                result['activity'].append(build_feed_item(entry))
+            return result
+        except:
+            return {}
+
+    @staticmethod
+    def get_activity(user):
+        try:
+            f = feedparser.parse('http://www.quora.com/' + user + '/rss')
+            activity = Activity()
+            for entry in f.entries:
+                activity_type = check_activity_type(entry)
+                if activity_type is not None:
+                    if activity_type == ACTIVITY_ITEM_TYPES.UPVOTE:
+                        activity.upvotes.append(build_feed_item(entry))
+                    elif activity_type == ACTIVITY_ITEM_TYPES.USER_FOLLOW:
+                        activity.user_follows.append(build_feed_item(entry))
+                    elif activity_type == ACTIVITY_ITEM_TYPES.WANT_ANSWER:
+                        activity.want_answers.append(build_feed_item(entry))
+                    elif activity_type == ACTIVITY_ITEM_TYPES.ANSWER:
+                        activity.answers.append(build_feed_item(entry))
+                    elif activity_type == ACTIVITY_ITEM_TYPES.REVIEW_REQUEST:
+                        activity.review_requests.append(build_feed_item(entry))
+            return activity
+        except:
+            return Activity()
+
+    @staticmethod
+    def get_followers(driver, user, usr, pwd):
+        followers = []
+        try:
+
+            url = 'http://www.quora.com/profile/' + user + '/followers'
+            driver.get(url)
+
+            #User.check_login(driver, usr, pwd)
+            #time.sleep(1)
+            #driver.get(url)
+
+            #time.sleep(1)
+
+
+
+            print "sleep well"
+            wait = WebDriverWait(driver, 5)
+            wait.until(EC.presence_of_element_located((By.XPATH, "//a[@ class='user']")))
+
+
+            tmp = BeautifulSoup(driver.page_source).find('div', class_='UserConnectionsFollowersList').find_all('a', class_='user', href=True)
+            print len(tmp)
+            for item in tmp:
+                followers.append(item['href'].split('/')[2])
+
+
+        except:
+            print "No more followers..."
+        finally:
+            return followers
+
+    @staticmethod
+    def check_login(driver, username, password):
+        try:
+            wait = WebDriverWait(driver, 1)
+            wait.until(EC.presence_of_element_located((By.XPATH, "//div[@ class='login']")))
+
+            usr = wait.until(EC.presence_of_element_located((By.XPATH,
+                "//input[@class='text header_login_text_box ignore_interaction' and @name='email']")))
+            pwd = wait.until(EC.presence_of_element_located((By.XPATH,
+                "//input[@class='text header_login_text_box ignore_interaction' and @name='password']")))
+
+
+            if usr is not None and pwd is not None:
+                usr.send_keys(username)
+                pwd.send_keys(password)
+            else:
+                print "One of is none"
+
+            loginbtn = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@type='submit' and @class='submit_button ignore_interaction' and @value='Login']")))
+            loginbtn.click()  ## next: reload the page by sending new request
+        except:
+            print "Already logged in..."
+
+class Activity:
+    def __init__(self, args=None):
+        self.upvotes = []
+        self.user_follows = []
+        self.want_answers = []
+        self.answers = []
+        self.review_requests = []
